1bf5520f2f20cc7af220c874834d5621062c973f
Nisha Shakya <157094446+NISHSKUMARI@users.noreply.github.com>
2025-06-08 17:42:32 +0530

    Add files via upload

---
+++ swap two num.py
@@ -0,0 +1,14 @@
+"""x=2
+y=3
+temp=x
+print("the value of variable",temp)
+x=y
+print("the value of  x is",x)
+y=temp
+print("the value of y is",y)"""
+
+x=12
+y=13
+x,y=y,x
+print(" the value of x is ",x)
+print("the value of y is ",y)
