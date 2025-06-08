e9c47cbfc74e9e6c564b43f6579b8e5578eb6cc0
Nisha Shakya <157094446+NISHSKUMARI@users.noreply.github.com>
2025-06-08 17:31:57 +0530

    Add files via upload

---
+++ face detection.py
@@ -0,0 +1,25 @@
+# pip install opencv-python
+# haarcascade_frontalface_default.xml
+
+import cv2
+
+a = cv2.CascadeClassifier("C:\Users\hp\Desktop\python project haarcascade_frontalface_default.xml")
+
+b = cv2.VideoCapture(0)
+
+while True:
+  c_rec,d_image = b.read()
+  gray= cv2.cvtColor(d_image, cv2.COLOR_BGR2GRAY)
+  f = a.detectMultiScale(gray, 1.3 ,6)
+  
+        
+  for (x1,y1,w1,h1) in f:
+    cv2.rectangle(d_image, (x1,y1), (x1+w1, y1+h1), (255,0,0), 5)
+    
+    cv2.imshow('img',d_image)
+    h = cv2.waitKey(40) & 0xff
+    if h == 40:
+      break
+    
+    b.release()
+    cv2.destroyAllWindows()
