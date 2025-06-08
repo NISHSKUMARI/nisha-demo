e318f48b93958613a85c0e630f1d90b0d05ae05f
Nisha Shakya <157094446+NISHSKUMARI@users.noreply.github.com>
2025-06-08 17:41:42 +0530

    Add files via upload

---
+++ square num.py
@@ -0,0 +1,7 @@
+"""num=64
+square=num**1/2
+print("square root of given number is", square)"""
+
+num1=int(input("enter the number:"))
+square=num1**1/2
+print("enter square of root of the given number",square)
